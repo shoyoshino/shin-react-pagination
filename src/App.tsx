cd8af22d7a06843bbba52dafe4bd134cd6c813d1
shoyoshino <shoyoshino@icloud.com>
2023-11-23 21:31:10 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Album from "./type";
+import Pagination from "./Pagination";
+
+function App() {
+  const [albums, setAlbums] = useState<Album[]>([]);
+
+  useEffect(() => {
+    const getAlbums = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+        .then((res) => res.json())
+        .then((albums) => setAlbums(albums));
+    };
+    getAlbums();
+  }, []);
+
+  return (
+    <>
+      <div className="App">
+        <Pagination albums={albums} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pagination.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./Pagination.css";
+import AlbumList from "./AlbumList";
+import Album from "./type";
+import ReactPaginate from "react-paginate";
+
+type Props = {
+  albums: Album[];
+};
+
+const Pagination = (props: Props) => {
+  const { albums } = props;
+
+  const itemsPerPage = 6; // 1ページで表示する画像数
+
+  const [itemsOffset, setItemsOffset] = useState(0);
+
+  const endOffset = itemsOffset + itemsPerPage;
+
+  const currentAlbums = albums.slice(itemsOffset, endOffset);
+
+  const pageCount = Math.ceil(albums.length / itemsPerPage);
+
+  const handlePageClick = (e: { selected: number }) => {
+    // console.log(e.selected);
+    const newOffset = (e.selected * itemsPerPage) % albums.length;
+    setItemsOffset(newOffset);
+  };
+
+  return (
+    <div className="albumWrapper">
+      <AlbumList albums={albums} currentAlbums={currentAlbums} />
+      <div className="paginateWrapper">
+        <ReactPaginate
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          nextLabel="next >"
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          previousLabel="< previous"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
